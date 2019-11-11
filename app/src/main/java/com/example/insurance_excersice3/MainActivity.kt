d8d240af1105dd29e7ef332201905708f8820f91
Ling Yeou Yi <enily_ling@hotmail.com>
2019-11-11 12:01:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/insurance_excersice3/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.insurance_excersice3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        spinnerAge.setOnItemClickListener(this)
+        buttonCalculate.setOnClickListener{
+            calculatePremium()
+        }
+    }
+
+    private fun calculatePremium(){
+        val position = spinnerAge.selectedItemPosition
+        val gender = radioGroupGender.checkedRadioButtonId
+
+        var premium: Int
+
+        if(position == 1){
+            premium = 60
+        }
+        else if (position == 2){
+            premium = 70
+        }
+        else if (position == 3){
+            premium = 90
+        }
+        else if (position == 4){
+            premium = 120
+        }
+        else {
+            premium = 150
+        }
+
+        if(gender == R.id.radioButtonMale){
+            //TODO: calculate premium for male
+
+        }
+        if(checkBox.isChecked){
+            //TODO: calculate premium for smoker
+        }
+    }
+}
